656e445f71234da6abce1603ca903cc1048dd50b
Billy Morris <wm59@st-andrews.ac.uk>
2022-11-02 16:20:43 +0000

    Started Java Script and added id tags

---
+++ hub/hub.js
@@ -0,0 +1,13 @@
+const KEY_RIGHT = 39;
+const KEY_LEFT = 37;
+const KEY_UP = 38;
+
+const HUB_WIDTH = 1250;
+const HUB_HEIGHT = 1050;
+
+const STATE = {
+    x_pos: 0,
+    y_pos: 0,
+    rotation: 0,
+    spaceship_width:50
+}
